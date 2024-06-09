4bef9fa1a1b290cef9ed06fbb1422fc9b7263f2a
joudxlll <joud_alrumayh@outlook.com>
2024-06-09 23:39:33 +0300

    day 9

--- HrApiDay09/src/main/java/org/example/controller/EmployeeController.java
+++ HrApiDay09/src/main/java/org/example/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 import org.example.dao.EmployeeDAO;
+import org.example.dao.JobDAO;
 import org.example.dto.EmployeeDto;
 import org.example.dto.EmployeeFilterDto;
 import org.example.exceptions.EmployeeServiceException;
@@ -10,6 +11,7 @@ import org.example.mappers.EmployeeMapper;
 import org.example.mappers.JobMapper;
 import org.example.models.Employees;
 import org.example.exceptions.DataNotFoundException;
+import org.example.models.Jobs;
 
 import java.net.URI;
 import java.sql.SQLException;
@@ -72,17 +74,24 @@ public class EmployeeController {
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, "text/csv"})
     public Response getEmployee(@PathParam("empId") int empId){
         try {
-            Employees emp;
+            Employees emp = null;
+            Jobs job;
             try {
-                emp= dao.selectEMP(empId);
+                emp = dao.selectEMP(empId);
+                job = new JobDAO().selectjob(emp.getJob_id());
+
             } catch (ClassNotFoundException e) {
-                throw new EmployeeServiceException("An unexpected error occurred while fetching employee details");
+                throw new RuntimeException(e);
             }
+
             if (emp == null) {
                 throw new DataNotFoundException("Employee " + empId + " Not found");
             }
+//            Location loc = locDao.selectLoc(dept.getLocationId());
 
-            EmployeeDto dto = EmployeeMapper.INSTANCE.toEmpDto(emp);
+//            DepartmentDto dto = DepartmentMapper.INSTANCE.toDeptDto(dept, loc);
+
+            EmployeeDto dto = EmployeeMapper.INSTANCE.toEmpDto(emp, job);
 //            dto.setEmployee_id(emp.getEmployee_id());
 //            dto.setHire_date(emp.getHire_date());
 //            dto.setJob_id(emp.getJob_id());
